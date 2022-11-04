cb97a69ef784eb81fc94931a8ace778016267d73
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-11-04 19:29:34 +0530

    Changes

--- provider.tf
+++ provider.tf
@@ -11,5 +11,7 @@ terraform {
 provider "newrelic" {
 #   account_id = 3627436  # Your New Relic accNRAK-KAA0NS59T13Eount ID
 #   api_key = "NRAK-JC08DMMZR3IF1DYMELC1KWEM571" # Your New Relic user key
+  account_id = secrets.NEW_RELIC_API_KEY  # Your New Relic accNRAK-KAA0NS59T13Eount ID
+  api_key = secrets.NEW_RELIC_ACCOUNT_ID # Your New Relic user key
   region  = "US"        # US or EU (defaults to US)
 }
