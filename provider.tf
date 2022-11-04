43be109d1b1af2ca9f5080f160e2305670c0268d
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-11-04 19:07:57 +0530

    initial

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    newrelic = {
+        source = "newrelic/newrelic"
+        #version = "3.2.1"
+        version = "3.5.0"
+    }
+  }
+}
+
+provider "newrelic" {
+  account_id = 3627436  # Your New Relic accNRAK-KAA0NS59T13Eount ID
+  api_key = "NRAK-JC08DMMZR3IF1DYMELC1KWEM571" # Your New Relic user key
+  region  = "US"        # US or EU (defaults to US)
+}
